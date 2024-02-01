2c3bc10bfd6f7a13179afe33cf4c791f4271209d
oceaneRoche <oceane.roche@lyceeastier.com>
2024-02-01 17:39:46 +0100

    3

--- src/main/java/fr/btsciel/td_convertisseur/HelloController.java
+++ src/main/java/fr/btsciel/td_convertisseur/HelloController.java
@@ -13,73 +13,60 @@ import java.util.ResourceBundle;
 
 public class HelloController implements Initializable {
     public AnchorPane anchorPane;
-    public TextField textField_Initial;
+    public TextField textField_Haut;
     public TextField textField_Final;
     public Button boutonConvertion;
     public ComboBox<ConversionDevice> comboSelection = new ComboBox<>();
     public RotateTransition rotation;
     public DecimalFormat df = new DecimalFormat("0.00");
     public ArrayList<ConversionDevice> conversionDevise = new ArrayList<>();
+    public Label label_Bas;
+    public Label label_Haut;
+
     public void Convertion() {
         try {
             ConversionDevice devise = comboSelection.getValue();
             double valeur_Conversion;
-
-            if (textField_Initial.isDisabled()) {
+            if (textField_Haut.isDisabled()) {
                 valeur_Conversion = Double.parseDouble(textField_Final.getText()) / devise.getTaux();
-                textField_Initial.setText(df.format(valeur_Conversion));
+                textField_Haut.setText(df.format(valeur_Conversion));
             } else {
-                valeur_Conversion = Double.parseDouble(textField_Initial.getText()) * devise.getTaux();
+                valeur_Conversion = Double.parseDouble(textField_Haut.getText()) * devise.getTaux();
                 textField_Final.setText(df.format(valeur_Conversion));
             }
-
         } catch (NumberFormatException e) {
             //alerteFormat();
         }
     }
-
     public void comboSelection() {
         ConversionDevice devise = comboSelection.getValue();
-        boolean isOddElement = comboSelection.getSelectionModel().getSelectedIndex() % 2 == 1;
+        boolean element = comboSelection.getSelectionModel().getSelectedIndex() % 2 == 1;
         rotation.setByAngle(180);
         rotation.play();
-        textField_Initial.setDisable(isOddElement);
-        textField_Final.setDisable(!isOddElement);
-
-        textField_Initial.clear();
+        textField_Haut.setDisable(element);
+        textField_Final.setDisable(!element);
+        textField_Haut.clear();
         textField_Final.clear();
     }
-
-    /*public void alerteFormat() {
-        TextField textField = textField_Initial.isDisabled() ? textField_Initial : textField_Final;
-        Alert dialogWindow = new Alert(Alert.AlertType.WARNING);
-        dialogWindow.setTitle("Error");
-        dialogWindow.setHeaderText(null);
-        dialogWindow.setContentText("Une mauvaise saisie '" + textField.getText() + "' n'est pas un nombre");
-        dialogWindow.showAndWait();
-    }*/
 
     public void fabriqueDonnees() {
         conversionDevise.add(new ConversionDevice("Euro/Dollars", "Euro", "Dollars US", 1.11));
         conversionDevise.add(new ConversionDevice("Dollars/Euro", "Dollars", "Euro", 1.11));
     }
 
-    /*public void initConvertion(ConversionDevice conversionDevise) {
-        label_Init.setText(conversionDevise.getSource());
-        label_Final.setText(conversionDevise.getCible());
-    }*/
+    public void initConvertion(ConversionDevice conversionDevise) {
+        label_Haut.setText(conversionDevise.getSource());
+        label_Bas.setText(conversionDevise.getCible());
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         rotation = new RotateTransition(Duration.seconds(0.5), boutonConvertion);
-        fabriqueDonnees();
-
         comboSelection.getItems().addAll(conversionDevise);
         comboSelection.setValue(conversionDevise.get(0));
-
-        //initConvertion(conversionDevise.get(0));
-
+        initConvertion(conversionDevise.get(0));
         boutonConvertion.setOnAction(event -> Convertion());
         comboSelection.setOnAction(event -> comboSelection());
+        fabriqueDonnees();
     }
 }
