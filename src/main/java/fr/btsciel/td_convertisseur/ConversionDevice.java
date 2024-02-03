9fe042759ff181eaa3a0ac010e86e51452b82765
oceaneRoche <oceane.roche@lyceeastier.com>
2024-02-03 18:02:32 +0100

    6

--- src/main/java/fr/btsciel/td_convertisseur/ConversionDevice.java
+++ src/main/java/fr/btsciel/td_convertisseur/ConversionDevice.java
@@ -11,35 +11,22 @@ public class ConversionDevice {
         this.taux=taux;
     }
 
-    public String getPrompt() {
-        return prompt;
+    public ConversionDevice() {
     }
 
-    public void setPrompt(String prompt) {
-        this.prompt = prompt;
+    public String getPrompt() {
+        return prompt;
     }
 
     public String getSource() {
         return source;
     }
 
-    public void setSource(String source) {
-        this.source = source;
-    }
-
     public String getCible() {
         return cible;
     }
 
-    public void setCible(String cible) {
-        this.cible = cible;
-    }
-
     public double getTaux() {
         return taux;
-    }
-
-    public void setTaux(double taux) {
-        this.taux = taux;
     }
 }
--- src/main/java/fr/btsciel/td_convertisseur/HelloController.java
+++ src/main/java/fr/btsciel/td_convertisseur/HelloController.java
@@ -12,60 +12,87 @@ import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class HelloController implements Initializable {
-    public AnchorPane anchorPane;
+    public Label label_Haut;
+    public Label label_Bas;
     public TextField textField_Haut;
-    public TextField textField_Final;
+    public TextField textField_Bas;
     public Button boutonConvertion;
-    public ComboBox<ConversionDevice> comboSelection = new ComboBox<>();
+    public ComboBox<String> comboSelection = new ComboBox<>();
+    public double valeurConversion;
     public RotateTransition rotation;
     public DecimalFormat df = new DecimalFormat("0.00");
-    public ArrayList<ConversionDevice> conversionDevise = new ArrayList<>();
-    public Label label_Bas;
-    public Label label_Haut;
-
-    public void Convertion() {
-        try {
-            ConversionDevice devise = comboSelection.getValue();
-            double valeur_Conversion;
-            if (textField_Haut.isDisabled()) {
-                valeur_Conversion = Double.parseDouble(textField_Final.getText()) / devise.getTaux();
-                textField_Haut.setText(df.format(valeur_Conversion));
-            } else {
-                valeur_Conversion = Double.parseDouble(textField_Haut.getText()) * devise.getTaux();
-                textField_Final.setText(df.format(valeur_Conversion));
-            }
-        } catch (NumberFormatException e) {
-        }
-    }
-
-    public void comboSelection() {
-        ConversionDevice devise = comboSelection.getValue();
-        boolean element = comboSelection.getSelectionModel().getSelectedIndex() % 2 == 1;
-        rotation.setByAngle(180);
-        rotation.play();
-        textField_Haut.setDisable(element);
-        textField_Final.setDisable(!element);
-        textField_Haut.clear();
-        textField_Final.clear();
-    }
-    public void fabriqueDonnees() {
-        conversionDevise.add(new ConversionDevice("Euro/Dollars", "Euro", "Dollars US", 1.11));
-        conversionDevise.add(new ConversionDevice("Dollars/Euro", "Dollars", "Euro", 1.11));
-    }
-
-    public void initConvertion(ConversionDevice conversionDevise) {
-        label_Haut.setText(conversionDevise.getSource());
-        label_Bas.setText(conversionDevise.getCible());
-    }
+    public ArrayList<ConversionDevice> conversionDevices = new ArrayList<>();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         rotation = new RotateTransition(Duration.seconds(0.5), boutonConvertion);
         fabriqueDonnees();
-        comboSelection.getItems().addAll(conversionDevise);
-        comboSelection.setValue(conversionDevise.get(0));
-        initConvertion(conversionDevise.get(0));
-        boutonConvertion.setOnAction(event -> Convertion());
-        comboSelection.setOnAction(event -> comboSelection());
+        remplirComboBoxAvecPrompts();
+        configurerValeurInitiale();
+        boutonConvertion.setOnAction(event -> effectuerConversion());
+        comboSelection.setOnAction(event -> gererSelectionCombo());
+    }
+
+    private void remplirComboBoxAvecPrompts() {
+        for (ConversionDevice conversionDevice : conversionDevices) {
+            comboSelection.getItems().add(conversionDevice.getPrompt());
+        }
+    }
+
+    private void configurerValeurInitiale() {
+        comboSelection.setValue(conversionDevices.get(0).getPrompt());
+        initConvertion(conversionDevices.get(0));
+    }
+
+    private void effectuerConversion() {
+        try {
+            ConversionDevice deviseSelectionnee = obtenirDeviseSelectionnee();
+            if (deviseSelectionnee != null) {
+                double valeurInput = textField_Haut.isDisabled() ?
+                        Double.parseDouble(textField_Bas.getText()) :
+                        Double.parseDouble(textField_Haut.getText());
+
+                valeurConversion = textField_Haut.isDisabled() ?
+                        valeurInput / deviseSelectionnee.getTaux() :
+                        valeurInput * deviseSelectionnee.getTaux();
+
+                if (textField_Haut.isDisabled()) {
+                    textField_Haut.setText(df.format(valeurConversion).replaceAll(",", "."));
+                } else {
+                    textField_Bas.setText(df.format(valeurConversion).replaceAll(",", "."));
+                }
+            }
+        } catch (NumberFormatException ignored) {
+            // Gérer l'exception (éventuellement log ou afficher un message)
+        }
+    }
+
+    private void gererSelectionCombo() {
+        boolean elementImpair = comboSelection.getSelectionModel().getSelectedIndex() % 2 == 1;
+        rotation.setByAngle(180);
+        rotation.play();
+        textField_Haut.setDisable(elementImpair);
+        textField_Bas.setDisable(!elementImpair);
+        textField_Haut.clear();
+        textField_Bas.clear();
+    }
+
+    private ConversionDevice obtenirDeviseSelectionnee() {
+        for (ConversionDevice devise : conversionDevices) {
+            if (comboSelection.getValue().equals(devise.getPrompt())) {
+                return devise;
+            }
+        }
+        return null;
+    }
+
+    private void fabriqueDonnees() {
+        conversionDevices.add(new ConversionDevice("Euro/Dollars", "Euro", "Dollars", 1.11));
+        conversionDevices.add(new ConversionDevice("Dollars/Euro", "Dollars", "Euro", 1.11));
+    }
+
+    private void initConvertion(ConversionDevice conversionDevice) {
+        label_Haut.setText(conversionDevice.getSource());
+        label_Bas.setText(conversionDevice.getCible());
     }
 }
